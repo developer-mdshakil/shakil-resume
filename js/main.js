d753988a16170bf269db4e1d0ea037d4e9dafc3f
developer-mdshakil <developershakil.cmt1819@gmail.com>
2023-07-16 10:57:12 +0600

    blog content site create

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// typed effect js code 
+var typed = new Typed(".auto-type", {
+    strings: ["Web Developer..", "Developer..", "Web Designer..", "Forntend Developer..", "Professional Coder"],
+    typeSpeed: 180,
+    backSpeed: 180,
+    loop: true
+})
